35571a086765a2368d52d801e15d800ae7be399c
PrakashAnpat <prakashp17696@gmail.com>
2022-09-19 14:09:15 +0530

    decorator function

--- demo1.py
+++ demo1.py
@@ -1,1 +1,14 @@
-print("hii, i am digambar")
+def abc(ele):
+    def xyz():
+        print("..........")
+        ele()
+        print("..........")
+        b = 10 * 10
+        print(b)
+    return xyz
+@abc
+def add ():
+    a = 10 + 10
+    print(a)
+add()
+
