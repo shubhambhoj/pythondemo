f075ea28bb408f7fadb2bf56c13d355d481ac86b
Abhishek <abhishekphadtare1998@gmail.com>
2022-09-23 15:03:23 +0530

    Add New Object #16

--- sting1.py
+++ sting1.py
@@ -1,20 +1,22 @@
-from tkinter import N
-
-
 class String:
     a = "class variable"
     b = "class variable1"
-    def __init__(self,p,n): # constructor
+    def __init__(self,p,n): # init Method & constructor
         self.name = p   
         self.age = n
     def show(self): #instance method
-        c = "Hello" #local variable
+        c = "Data" #local variable
         print(c)
-        print("Name:-",self.name)
-        print("Age:-",self.age)
+        print(f"Name:-{self.name}")
+        print(f"Age:-{self.age}")
     @classmethod # decorator
     def show_variable(cls): # class method
-        print(cls.a,cls.b)
-obj = String("Prakash",27)
-obj.show()
+        print("1st  {} and 2nd  {}".format(cls.a,cls.b))
+obj1 = String("Prakash",27)
+obj2 = String("Abhishek",24)
 
+obj1.show()
+print()
+obj2.show()
+print()
+obj2.show_variable()
