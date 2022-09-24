7a902dbd930960a40a27fa90eba7a2855832df25
Abhishek <abhishekphadtare1998@gmail.com>
2022-09-24 09:02:55 +0530

    add input function #2

--- function.py
+++ function.py
@@ -1,7 +1,7 @@
 def add ():
-    a = 10 
-    b = 15
+    a = int(input("Enter the 1st No - :")) 
+    b = int(input("Enter the 2nd No - :"))
     c = a + b
-    print(c)
+    print("The Addition is -:{}".format(c))
 
 add()    
